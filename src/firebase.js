06166dd4485e87c1275752e14c09981fde984522
Yahgozie <nyahgozie@gmail.com>
2021-09-15 13:00:18 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCogpDobD9mkAqYnYuZLL59VoreD6LLxms",
+    authDomain: "clone-4bae1.firebaseapp.com",
+    projectId: "clone-4bae1",
+    storageBucket: "clone-4bae1.appspot.com",
+    messagingSenderId: "181531858505",
+    appId: "1:181531858505:web:3b76056b111ffc94fe1198",
+    measurementId: "G-4CDZ1F8PDN"
+  };
