ccfad22192311553b5227df82827673f8db9438f
Yahgozie <nyahgozie@gmail.com>
2021-09-17 11:23:20 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import RecommendedVideos from "./RecommendedVideos";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 
 function App() {
   return (
-    <div className="app">
+    <Router>
       <Header />
-      <div className="app_page">
-        <Sidebar />
-        <RecommendedVideos/>
-      </div>
-    </div>
+      <Switch>
+        <Route path="/search/:searchTerm">
+          <div className="app_page">
+            <Sidebar />
+            <SearchPage />
+          </div>
+        </Route>
+        <Route path="/">
+          <div className="app_page">
+            <Sidebar />
+            <RecommendedVideos />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
